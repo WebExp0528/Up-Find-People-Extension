9dc7a24d9114b72b250b0200ad4cff004e3c6834
WebEXP0528 <app.dev0528@gmail.com>
2020-12-01 10:55:20 -0800

    minor fixes

--- src/content/index.js
+++ src/content/index.js
@@ -13,7 +13,7 @@ class Main extends React.Component {
     render() {
         return (
             <div className="my-extension">
-                <h1>Hello world - My first Extension</h1>
+                <h1></h1>
             </div>
         );
     }
